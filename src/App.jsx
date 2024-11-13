bd8d34502e711763924895eee151f4a75f5ff81c
Ahmad alzein <ahmad@basma.com>
2024-11-14 00:18:00 +0200

    Add xbox and playstation pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import GamesList from "./components/GamesList"
-
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import XboxGameList from "./components/XboxGameList"
+import PlayStationGameList from "./components/PlayStationGameList"
 function App() {
 
   return (
     <>
-      {/* <GamePassGamesList /> */}
-      <GamesList />
+      <Router>
+        <Routes>
+          <Route path='/' element={<GamesList/>} />
+          <Route path='/xbox' element={<XboxGameList/>} />
+          <Route path='/ps' element={<PlayStationGameList/>} />
+        </Routes>
+      </Router>
     </>
   )
 }
