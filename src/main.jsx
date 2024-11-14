7d7a98dd9de874b89836c245fa22c407aabea39b
Ahmad alzein <ahmad@basma.com>
2024-11-14 17:03:46 +0200

    Add helmet

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,15 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Helmet } from 'react-helmet';
 
 createRoot(document.getElementById('root')).render(
+    <>
+    <Helmet>
+      <title>Xbox and Playstation Game Library</title>
+      <meta name="description" content="Explore Common Xbox and Playstation games" />
+      <link rel="icon" href="/xb-ps.jpg" type="image/svg+xml" />
+    </Helmet>
     <App />
+  </>
 )
